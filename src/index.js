8ea393d8829d62c4a99d2fb97006c8b791222755
Max <maxicespedes7@hotmail.com>
2019-07-21 00:17:29 +0200

    Copy pasterino amigo funcionaaaaaaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+const fs = require('fs');
+
 const difference = require('lodash/difference');
 const { getAllFilesFrom } = require('../utils/file');
+
+const getDestinationFolder = destFolder => {
+  const pathi = destFolder.split('/');
+  return `/baky/from_${pathi[pathi.length - 1]}`;
+};
+
+const createDestinationFolder = (base, destFolder) => {
+  const dir = `${base}${getDestinationFolder(destFolder)}`;
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
 
 const baky = async ({ folders }) => {
   console.info('Initiating baky...');
   const folderPromises = folders.map(folder => getAllFilesFrom(folder, true));
   return Promise.all(folderPromises)
     .then(res => {
-      // console.log(res);
       res.forEach((result, ix) => {
         console.info('current folder:', folders[ix]);
 
@@ -14,8 +28,6 @@ const baky = async ({ folders }) => {
           if (ix === ixComparator) return;
           console.info('comparing with folder', folders[ixComparator]);
 
-          // console.info('files in comparator folder', res[ixComparator].list);
-          // const pairNameSizeComparatorFolder =
           const diff = difference(result.list, res[ixComparator].list);
           if (diff.length) {
             console.warn(
@@ -24,11 +36,48 @@ const baky = async ({ folders }) => {
               'in folder',
               folders[ixComparator],
             );
+
+            console.info(`${diff.length} files are not present.`);
+
+            console.info('Initiating copy');
+
+            const copyPromises = [];
+
+            diff.forEach(missingFile => {
+              const copyPromise = new Promise((resolve, reject) => {
+                createDestinationFolder(folders[ixComparator], folders[ix]);
+
+                const missingFileInfo = result.details.find(
+                  fileInfo => fileInfo.id === missingFile,
+                );
+                console.log('missingFileInfo', missingFileInfo);
+                const source = missingFileInfo.filePath;
+                console.log('source', source);
+                const dest = `${folders[ixComparator]}${getDestinationFolder(
+                  folders[ix],
+                )}/${missingFileInfo.file}`;
+
+                console.log('dest', dest);
+
+                fs.copyFile(source, dest, err => {
+                  if (err) {
+                    console.error(`Error copying:${source}
+                    to: ${dest}
+                    error:${err}`);
+
+                    reject();
+                  }
+
+                  resolve();
+                });
+              });
+
+              copyPromises.push(copyPromise);
+            });
+
+            return Promise.all(copyPromises);
           }
         });
-        // console.info('folder', folders[ix], 'files', result.details);
-
-        // para cada
       });
     })
     .catch(err => console.log(err));
